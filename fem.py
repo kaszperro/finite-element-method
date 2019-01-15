756f1795bc93d243d33cbc3be494fd4de08b8372
Kasper Sapała <kasper.sapala@gmail.com>
2019-01-15 20:47:32 +0100

    update

--- fem.py
+++ fem.py
@@ -1,6 +1,7 @@
 from typing import Callable
-import numpy as np
 
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.integrate import quad
 
 
@@ -8,9 +9,8 @@ def get_base_function(k):
     return lambda x: max((1 - abs(x * n - k)), 0)
 
 
-def get_base_function_der(i):
-    return lambda x: n * (((i - 1) / n) <= x) * (x < (i / n)) - n * ((i / n) < x) * (
-            x <= ((i + 1) / n))
+def get_base_function_der(k):
+    return lambda x: 0 if x < (k - 1) / n or x > (k + 1) / n else n if x < k / n else -n
 
 
 def calc_b(u_der: Callable, v_der: Callable, u: Callable, v: Callable, start: float, end: float) -> float:
@@ -28,16 +28,9 @@ def calc_l(v: Callable, start, end) -> float:
     return -gamma * v(0) + quad(lambda x: function_f(x) * v(x), start, end)[0]
 
 
-def calc_b_in_matrix(i, j) -> float:
-    if abs(i - j) > 1:
-        return 0
-
-    if abs(i - j) == 1:
-        start = max(0.0, min(i, j) / n)
-        end = min(1.0, max(i, j) / n)
-    else:
-        start = max(0.0, (i - 1) / n)
-        end = min(1.0, (i + 1) / n)
+def calc_b_in_matrix(i: int, j: int) -> float:
+    start = max(0.0, (i - 1) / n)
+    end = min(1.0, (j + 1) / n)
 
     return calc_b(
         get_base_function_der(i),
@@ -57,19 +50,19 @@ def get_u() -> Callable:
 
     for i in range(0, n):
         for j in range(0, n):
-            matrix[i][j] = calc_b_in_matrix(i, j)
+            matrix[i][j] = calc_b_in_matrix(j, i)
 
     right_matrix = np.empty(n)
 
     for i in range(0, n):
-        right_matrix[i] = calc_l(get_base_function(i), max(0.0, i / n - 1.0 / n), min(1.0, i / n + 1.0 / n)) - calc_b(
+        right_matrix[i] = calc_l(get_base_function(i), max(0.0, (i - 1) / n), min(1.0, (i + 1) / n)) - calc_b(
             lambda x: u1,
             get_base_function_der(i),
             shift,
-            get_base_function(i), max(0.0, i / n - 1.0 / n), min(1.0, i / n + 1.0 / n)
+            get_base_function(i), max(0.0, (i - 1) / n), min(1.0, (i + 1) / n)
         )
 
-    part_result = np.linalg.solve(matrix, right_matrix)
+        part_result = np.linalg.solve(matrix, right_matrix)
 
     return lambda x: shift(x) + sum(
         u * get_base_function(iu)(x) for iu, u in enumerate(part_result)
@@ -77,21 +70,26 @@ def get_u() -> Callable:
 
 
 if __name__ == '__main__':
-    def function_a(x): return 1
+    def function_a(x): return 0
 
 
-    def function_b(x): return 0
+    def function_b(x): return 1
 
 
     def function_c(x): return 0
 
 
-    def function_f(x): return 0
+    def function_f(x): return 3 * np.sin(x*10) * x
 
 
     beta = 0
     gamma = 0
-    u1 = 1
-    n = 1000
+    u1 = 1 / 2
+    n = 100
 
-    print(get_u()(0.9))
+    u_function = get_u()
+
+    x_space = np.linspace(0, 1, 50)
+    y_res = [u_function(x) for x in x_space]
+    plt.plot(x_space, y_res)
+    plt.show()
